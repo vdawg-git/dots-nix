acdfcdb0b95d51e9297d0dfc97cab9a256067040
VDawg <vdawg@tuta.io>
2025-07-08 22:11:15 +0200

    disable plymouth for sure

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -13,12 +13,7 @@
   # boot.initrd.availableKernelModules = [ "i915" ];
   # boot.initrd.kernelModules          = [ "i915" ];
   boot.consoleLogLevel = 3;
-  # boot.plymouth = {
-  #   enable = true;
-  #   font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
-  #   themePackages = [ pkgs.catppuccin-plymouth ];
-  #   theme = "catppuccin-macchiato";
-  # };
+  boot.plymouth.enable = false;
 
   environment.systemPackages = with pkgs; [ greetd.tuigreet ];
 }
