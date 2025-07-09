4e6cc6e7949e33ed95d3f798e63fcb9852ee1bbd
VDawg <vdawg@tuta.io>
2025-07-09 10:47:02 +0200

    add comma, manix

--- system/nixx.nix
+++ system/nixx.nix
@@ -3,6 +3,8 @@
 {
   environment.systemPackages = with pkgs; [
     nixfmt-rfc-style
+    comma # Run anything instantly with `, some-app`
+    manix # Easy NixOs docs searcher
   ];
 
   programs.nix-index.enable = true;
