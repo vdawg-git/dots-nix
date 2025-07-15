743b46dacbd8b51a82c42845df88e33a2fee886a
VDawg <vdawg@tuta.io>
2025-07-15 15:36:05 +0200

    fixx

--- system/pam.nix
+++ system/pam.nix
@@ -1,8 +1,8 @@
 { ... }:
 
 {
-  services.gnome3.gnome-keyring.enable = true;
-  services.gnome3.seahorse.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.gnome.seahorse.enable = true;
 
   # What is starting this. I dont want this
   security.pam.services.kwallet = {
