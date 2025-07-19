72fdb052fcfbb56aca58a6ca2e94f5ad08316151
VDawg <vdawg@tuta.io>
2025-07-19 18:48:56 +0200

    gruvbox theme boot

--- modules/system/theme.nix
+++ modules/system/theme.nix
@@ -2,22 +2,22 @@
   console = {
     earlySetup = true;
     colors = [
-      "000000"
-      "b85651"
-      "a6da95"
-      "eed49f"
-      "8aadf4"
-      "f5bde6"
-      "8bd5ca"
-      "cad3f5"
-      "5b6078"
-      "ed8796"
-      "a6da95"
-      "eed49f"
-      "8aadf4"
-      "f5bde6"
-      "8bd5ca"
-      "a5adcb"
+      "000000" # black
+      "ea6962" # red
+      "a9b665" # green
+      "d8a657" # yellow
+      "7daea3" # blue
+      "d3869b" # magenta
+      "89b482" # cyan
+      "c5b18d" # light gray
+      "5a524c" # dark gray (bright black)
+      "ea6962" # bright red
+      "a9b665" # bright green
+      "d8a657" # bright yellow
+      "7daea3" # bright blue
+      "d3869b" # bright magenta
+      "89b482" # bright cyan
+      "ddc7a1" # white
     ];
   };
 }
