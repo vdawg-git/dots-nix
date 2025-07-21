956e1c5ed7b5cae36de4d817dde60fab17dfd298
VDawg <vdawg@tuta.io>
2025-07-21 13:38:59 +0200

    easier postgres

--- modules/system/postgres.nix
+++ modules/system/postgres.nix
@@ -5,8 +5,16 @@
     enable = true;
     ensureDatabases = [ "mydatabase" ];
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+      # "local" is for Unix domain socket connections only
+      local   all             all                                     trust
+
+      # IPv4 local connections:
+      host    all             all             127.0.0.1/32            trust
+
+      # IPv6 local connections:
+      host    all             all             ::1/128                 trust
     '';
   };
 }
