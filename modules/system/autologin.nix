e0e95d7248e63553f0eb99b4a07765e175f46cdb
VDawg <vdawg@tuta.io>
2025-08-09 16:05:55 +0200

    cleanup

--- modules/system/autologin.nix
+++ modules/system/autologin.nix
@@ -1,12 +1,13 @@
 { pkgs, lib, ... }:
 
 {
-  services.xserver.displayManager.lightdm.enable = false; # Disable default display manager (ensure no other DMs are enabled)
+  services.xserver.displayManager.lightdm.enable = false;
   services.getty.autologinUser = "vdawg"; # Auto-login user on boot
+
   environment.loginShellInit = ''
     # Launch Hyprland on TTY1, return to TTY when exiting
     if [ "$(tty)" = "/dev/tty1" ]; then
-      ${lib.getExe' pkgs.hyprland} # Use `exec Hyprland` to auto-restart on exit/crash instead
+      ${lib.getExe' pkgs.hyprland "Hyprland"} # Use `exec Hyprland` to auto-restart on exit/crash instead
     fi
   '';
 }
