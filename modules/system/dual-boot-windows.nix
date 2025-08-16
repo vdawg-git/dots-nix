993122374ec610daf81fcf0db1ead60493fb362b
VDawg <vdawg@tuta.io>
2025-08-16 19:12:10 +0200

    .

---
+++ modules/system/dual-boot-windows.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+
+{
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
+}
--- modules/system/pkgs-base.nix
+++ modules/system/pkgs-base.nix
@@ -39,7 +39,7 @@ let
     telegram-desktop
     tesseract
     vesktop
-    vscode.fhs
+    vscode
     walker
     wireguard-tools
   ];
