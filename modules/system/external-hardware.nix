95a20260b23b6254ec09631ff2c9c5379040148f
VDawg <vdawg@tuta.io>
2025-09-07 21:02:57 +0200

    try to make external ssd work

--- modules/system/external-hardware.nix
+++ modules/system/external-hardware.nix
@@ -1,5 +1,9 @@
-{
-  # To make usb sticks work
+{pkgs, ...}: {
+  # To make usb sticks under Nautilus work
   services.gvfs.enable = true;
   services.udisks2.enable = true;
+  services.devmon.enable = true;
+
+  # Stuff for filesystems
+  environment.systemPackages = with pkgs; [udisks nfs-utils exfatprogs];
 }
