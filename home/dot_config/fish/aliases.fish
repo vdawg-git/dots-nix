90e788029930849b24bf6b6c99b4406fd22c3855
VDawg <vdawg@tuta.io>
2025-10-24 12:23:49 +0200

    use ni for pnpm shortcuts

--- home/dot_config/fish/aliases.fish
+++ home/dot_config/fish/aliases.fish
@@ -1,13 +1,6 @@
-abbr pn "pnpm"
-abbr pnd "pnpm dev"
-abbr pnb "pnpm build"
-abbr pni "pnpm install"
+abbr nrd "nr dev"
+abbr nrb "pnpm build"
 
-abbr xclip "xclip -selection c"
-abbr cz "chezmoi"
-abbr pac "sudo pacman -S "
-abbr pacr "sudo pacman -Rs "
-abbr yays "yay -Syu "
 abbr icat "kitty +kitten icat"
 abbr c. "code ."
 abbr n. "nvim ."
@@ -18,9 +11,6 @@ abbr mvc "mullvad connect"
 abbr mvs "mullvad status"
 
 abbr lsg "ls | rg "
-
-abbr wgu "wg-quick up job "
-abbr wgd "wg-quick down job "
 
 abbr g "git"
 abbr gitc "git-clone-and-cd"
