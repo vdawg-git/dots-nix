3a0408a0ae5fce9ca72acf236f5a4f67481c4fac
VDawg <vdawg@tuta.io>
2025-11-20 11:19:04 +0100

    try fixing autologin again

--- modules/system/autologin.nix
+++ modules/system/autologin.nix
@@ -9,13 +9,13 @@
   environment.loginShellInit = ''
     # Launch Hyprland on TTY1, return to TTY when exiting
     if [ "$(tty)" = "/dev/tty1" ]; then
-      dbus-update-activation-environment --systemd DISPLAY
+      dbus-update-activation-environment --systemd --all
 
       ${lib.getExe' pkgs.hyprland "Hyprland"} # Use `exec Hyprland` to auto-restart on exit/crash instead
     fi
   '';
 
-  # Hopefully fixes the keyring not unlocking by default https://github.com/NixOS/nixpkgs/issues/174099#issuecomment-1201697954
+  # Still doesnt fix stuff, but helps a bit I think https://github.com/NixOS/nixpkgs/issues/174099#issuecomment-1201697954
   services.xserver.displayManager.sessionCommands = ''
     ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
   '';
