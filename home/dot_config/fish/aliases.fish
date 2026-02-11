fdf3150548ea8fd2b372793c21cb03451831e0f8
VDawg <vdawg@tuta.io>
2026-02-11 18:47:02 +0100

    update fish config

--- home/dot_config/fish/aliases.fish
+++ home/dot_config/fish/aliases.fish
@@ -12,14 +12,19 @@ abbr mvs "mullvad status"
 
 abbr lsg "ls | rg "
 
-abbr g "git"
-abbr gitc "git-clone-and-cd"
-abbr gitcf "git-clone-and-cd-fast"
+abbr g git
+abbr gitc git-clone-and-cd
+abbr gitcf git-clone-and-cd-fast
 abbr gitf "git add -A && git commit -m 'commit save point' && git push"
 abbr gitp "git pull"
 
-abbr nixs "nix-shell"
+abbr nixs nix-shell
 
-abbr frb "flutter_rust_bridge_codegen"
+abbr frb flutter_rust_bridge_codegen
 
 abbr sepoku "systemctl poweroff"
+
+abbr jd "just dev"
+
+# No need to install the whole Postgres package for that
+alias psql="nix shell nixpkgs#postgresql --command psql"
