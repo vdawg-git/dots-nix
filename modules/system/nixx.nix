e6ead0aa106ea0b392b4eb4c6dfb8af3d9cb050b
VDawg <vdawg@tuta.io>
2026-04-13 22:14:12 +0300

    add nix db update script

--- modules/system/nixx.nix
+++ modules/system/nixx.nix
@@ -1,17 +1,37 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  # For some reason the nix-index module just isnt working well with comma, so lets do it manually.
+  updateNixIndex = pkgs.writeShellScriptBin "update-nix-index" ''
+    set -euo pipefail
+
+    filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+
+    cache_dir="$HOME/.cache/nix-index"
+
+    mkdir -p "$cache_dir"
+    cd "$cache_dir"
+
+    ${pkgs.wget}/bin/wget -q -N \
+      "https://github.com/nix-community/nix-index-database/releases/latest/download/$filename"
+
+    ln -f "$filename" files
+
+    echo "✔ nix-index database updated"
+  '';
+in {
   environment.systemPackages = with pkgs; [
     alejandra # Fast nice nix code formatter
     manix # Easy NixOS docs searcher
     any-nix-shell # Use Fish after going into a shell
 
     comma # Run anything instantly with `, some-app`
+
+    updateNixIndex
   ];
 
   # Idk why, but this failed
   programs.command-not-found.enable = false;
   # And this works hopefully
   programs.nix-index.enable = true;
-
 
   programs.nh = {
     enable = true;
