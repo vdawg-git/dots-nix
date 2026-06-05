37ab18e9cbe1deb3a9495552725b4364941d3806
VDawg <vdawg@tuta.io>
2026-06-05 18:44:08 +0300

    theme stuff

---
+++ home/setup-theme.sh
@@ -0,0 +1,8 @@
+git clone git@github.com:vinceliuice/Colloid-icon-theme.git /tmp/icon-theme
+cd /tmp/icon-theme &&  ./install.sh --bold --scheme gruvbox
+
+git clone https://github.com/vdawg-git/Colloid-gtk-theme /tmp/gtk-theme
+git switch transparency
+cd /tmp/gtk-theme && ./install.sh --theme orange --libadwaita --tweaks gruvbox rimless black --color dark
+
+
